7125686e97b9f2e84023c2e6996ee26a89d6cd5e
Madison Kerndt <madison.kerndt@gmail.com>
2016-10-13 12:53:07 -0600

    close issue two display individual reminder

--- app/routes/reminders/reminder.js
+++
@@ -1,9 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.Route.extend({
-  model(params) {
-    console.log('testing route')
-    console.log(params);
-    return this.get('store').find('reminder', params.reminder.id);
-  }
-});
