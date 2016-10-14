d462d73fc593728957f459e774c49d80c5020e8d
Brett Hevia <brett.hevia@gmail.com>
2016-10-14 12:58:35 -0600

    clear inputs

--- app/routes/reminders/new.js
+++ app/routes/reminders/new.js
@@ -1,15 +1,27 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-    // title : '',
-    // notes : '',
-    // date : '',
+    model() {
+      return Ember.Object.create()
+    },
     actions: {
-      testAction: function() {
+      addNewReminder: function(model) {
+        let controller = this.controllerFor('reminders.new');
+
+        let reminderDate;
+        if(model.date) { reminderDate = new Date(model.date) }
+        else { reminderDate = new Date() }
         this.store.createRecord('reminder', {
-            title: this.title
-            // notes: this.notes
-          });
+          title: model.title,
+          notes: model.notes,
+          date: reminderDate
+        }).save().then(function() {
+          // this --> if moved to controller
+          controller.set('model', Ember.Object.create())
+        });
       }
     }
   });
+
+
+  // what is the difference of programing with return vs without (using return is functional programing )
