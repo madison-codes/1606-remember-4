075bc3c20d7cc349cb7d9ce553e1cdb295a0a670
Madison Kerndt <madison.kerndt@gmail.com>
2016-10-15 15:00:26 -0600

    adds new reminder as controller

--- app/routes/reminders/new.js
+++ app/routes/reminders/new.js
@@ -2,24 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
     model() {
-      return Ember.Object.create()
-    },
-    actions: {
-      addNewReminder: function(model) {
-        let controller = this.controllerFor('reminders.new');
-
-        let reminderDate;
-        if(model.date) { reminderDate = new Date(model.date) }
-        else { reminderDate = new Date() }
-        this.store.createRecord('reminder', {
-          title: model.title,
-          notes: model.notes,
-          date: reminderDate
-        }).save().then(function() {
-          // this --> if moved to controller
-          controller.set('model', Ember.Object.create())
-        });
-      }
+      return Ember.Object.create();
     }
   });
 
