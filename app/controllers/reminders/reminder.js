db9d86f999d872984476655308397b0cfe3ddcab
Madison Kerndt <madison.kerndt@gmail.com>
2016-10-17 15:02:34 -0600

    error in model.title

--- app/controllers/reminders/reminder.js
+++ app/controllers/reminders/reminder.js
@@ -2,16 +2,18 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   actions: {
-    addNewReminder: function(model) {
+    editReminder: function() {
+      this.toggleProperty('isEditing');
+      return false;
+    },
+
+    saveEdited: function(model) {
       this.store.createRecord('reminder', {
         title: model.title,
         notes: model.notes,
         date: model.date || new Date()
       })
-      .save()
-      .then(function(){
-        model.setProperties({ title: '', date: '', notes: '' });
-     });
+      .save();
     }
   }
 });
